dc55f56ecde763250ec1d4a91a112848bf1123ec
Ash <ashvin.kasture12345@gmail.com>
2019-12-02 19:31:08 +0530

    added and fixed book drop effect

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let book = $('.bookImg');
 
+var count = 0;
 
-$(document).ready(function(){
-    $("#knowledge").on("click",function()
-    {
+$(document).ready(function () {
+    $("#knowledge").on("click", function () {
         console.log("Get Out");
-        $(".bookImg").hide();
-        $(".bookImg").attr("src","src/images/ani.gif");
-        setTimeout(function(){
-            $(".bookImg").show();
-        },2000);
-        setTimeout(function(){
-            $(".bookImg").attr("src","src/images/done.gif")
-        },7000);
+        if (count == 0) {
+            $(".bookImg1").show();
+            $(".bookImg1").attr("src", "src/images/final.gif");
+            setTimeout(function () {
+                $(".bookImg1").attr("src", "src/images/done.gif")
+            }, 7000);
+            count++;
+        }
+        else if(count == 1)
+        {
+            $(".bookImg2").show();
+            $(".bookImg2").attr("src", "src/images/final.gif");
+            setTimeout(function () {
+                $(".bookImg2").attr("src", "src/images/done.gif")
+            }, 7000);
+            count++;
+        }
+        else if (count == 2){
+            count = 0;
+            $(".bookImg1").hide();
+            $(".bookImg2").hide();
+        }
+
     })
 })
